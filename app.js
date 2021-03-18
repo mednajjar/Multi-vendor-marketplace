23457c55fea34fecbed6e2a3bd8e6c7e11226501
Mohammed Najjar <najjarmedd@gmail.com>
2021-03-18 12:12:59 +0100

    create database with local and onlign connection

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+mongoose.connect(process.env.LOCAL_URI,{useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log('Database connected!'))
+.catch(()=>console.log('Faild to connect with database!'))
 
 
 module.exports = app;
